9d7c8c3d0b1ce31a67ac9ea48b5972805ebc987e
Alexander <alexander.dean.paulsell@gmail.com>
2019-05-27 20:49:05 -0700

    basic menu look/ navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { WebsiteComponent } from './website/website.component';
+import { AboutComponent } from './about/about.component';
+import { FrontendComponent } from './frontend/frontend.component';
+import { BackendComponent } from './backend/backend.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'website', component: WebsiteComponent },
+  { path: 'frontend', component: FrontendComponent },
+  { path: 'backend', component: BackendComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { WebsiteComponent } from './website/website.component';
+import { FrontendComponent } from './frontend/frontend.component';
+import { BackendComponent } from './backend/backend.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    WebsiteComponent,
+    FrontendComponent,
+    BackendComponent,
   ],
   imports: [
     BrowserModule,
